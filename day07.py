c5dc6266a12be0ad1a3d6a6b0afa30aba96303db
sara-shiho <saraho2021@u.northwestern.edu>
2021-12-15 18:13:29 -0600

    simplify part a

--- day07.py
+++ day07.py
@@ -1,4 +1,5 @@
 """Intuition for Day 07:
+part a's best position is just the median
 part b requires triangle sum ie the sum of the range
 """
 from aocd import get_data, submit
@@ -9,14 +10,13 @@ YEAR = 2021
 
 # get data
 raw_data = get_data(day=DAY, year=YEAR)
-data = np.array([int(x) for x in raw_data.split(',')])
+data = [int(x) for x in raw_data.split(',')]
 
 unique_positions = np.unique(data)
 
 # part a
-distances_a = [sum(abs(data - position))
-               for position in unique_positions]
-submit(min(distances_a), part='a', day=DAY, year=YEAR)
+distance_a = sum(np.abs(data - np.median(data)))
+submit(min(distance_a), part='a', day=DAY, year=YEAR)
 
 # part b
 distances_b = [sum([sum(range(d + 1)) for d in np.abs(data - position)])
